8f78ccce240a1b148e73fee9889651389365f500
Matus <matesfila@gmail.com>
2021-04-20 16:12:31 +0200

    prvé hry s timerom a podobne: - zavedenie timera prvá verzia: rozdelenie sequencera na Sequencer_sync a Sequencer_timer - možnosť prehrávať v loope pre každý sequencer (Sequencer.playInLoop()) - sequencer môže obsahovať viacero nástrojov, ktoré bude prehrávať (atribút Sequencer.instruments)

--- Afronom.py
+++ Afronom.py
@@ -1,89 +1,128 @@
 from time import sleep
-from machine import Pin
-import math
+from machine import Pin, Timer
 
 
 class Instrument:
-    
+
     def playNormal(self, note):
         pass
 
     def playAccent(self, note):
         pass
-        
+
     def onBarStart(self):
         pass
-    
+
     def onBarEnd(self):
         pass
-        
+
     def play(self, note):
         if note == "x":
-            playNormal(self, note);
+            return self.playNormal(note)
         elif note == "X":
-            playAccent(self, note);
+            return self.playAccent(note)
         elif note == ".":
-            pass
+            return 0
 
 
 class Printer(Instrument):
 
     def playNormal(self, note):
-        print("x");
-        return 0;
+        print("p", end="")
+        return 0
 
     def playAccent(self, note):
-        print("X");
-        return 0;
+        print("P", end="")
+        return 0
 
-    #def onBarEnd(self):
+    # def onBarEnd(self):
     #    print("---");
 
-        
+
 class LedLighter(Instrument):
-    
-    
+
     def __init__(self):
         self.led = Pin(25, Pin.OUT)
+        # self.timer = Timer()
+
+    # def playNormal(self, note):
+    #     self.note = note
+    #     # self.timer.init(freq=0, mode=Timer.ONE_SHOT, callback=self.playNormal_internal)
 
     def playNormal(self, note):
-        l = self.led
-        l.on();
-        sleep(0.003);
-        l.off();
-        return 0.003;
+        led = self.led
+        led.on()
+        sleep(0.007)
+        led.off()
+        return 0.007
 
     def playAccent(self, note):
-        l = self.led
-        l.on();
-        sleep(0.07);
-        l.off();
-        return 0.07;
+        led = self.led
+        led.on()
+        sleep(0.07)
+        led.off()
+        return 0.07
 
-        
+
 class Sequencer:
-    
-    TEMPO_BPM = 150;
+    TEMPO_BPM = 130
 
     def __init__(self):
+        self.instruments = []
+
+    def withInstruments(self, instruments):
+        self.instruments = instruments
+        return self
+
+    def playBar(self, bar):
+        pass
+
+    def playInLoop(self, bar):
+        pass
+
+
+class Sequencer_sync(Sequencer):
+
+    def __init__(self):
+        super().__init__()
         self.tempo_sec = 60 / self.TEMPO_BPM
 
-    def playBar(self, bar, instrument):
-        instrument.onBarStart();
+    def playBar(self, bar):
+        for instr in self.instruments:
+            instr.onBarStart()
+
         for note in bar:
-            if note == "X":
-                seconds = instrument.playAccent(note)
-                sleep(self.tempo_sec / 2 - seconds);
-            if note == "x":
-                seconds = instrument.playNormal(note)
-                sleep(self.tempo_sec / 2 - seconds);
-            elif note == ".":
-                sleep(self.tempo_sec / 2 - seconds);
-        instrument.onBarEnd();
+            seconds = 0
+            for instr in self.instruments:
+                seconds = seconds + instr.play(note)
+            sleep(self.tempo_sec / 2 - seconds)
+            # sleep(self.tempo_sec / 2)
+
+        for instr in self.instruments:
+            instr.onBarEnd()
+
+    def playInLoop(self, bar):
+        while True:
+            self.playBar(bar)
 
 
-sequencer = Sequencer()
-instrument = LedLighter()
-            
-while True:
-    sequencer.playBar("X.xx.xx.X.x.x.x.", instrument);
+class Sequencer_timer(Sequencer):
+
+    def __init__(self):
+        super().__init__()
+        self.timer = Timer()
+
+    def onEightNote(self, timer):
+        for instr in self.instruments:
+            instr.play("x")
+
+    def playBar(self, bar):
+        self.timer.init(freq=self.TEMPO_BPM / 60, mode=Timer.PERIODIC, callback=self.onEightNote)
+
+    def playInLoop(self, bar):
+        self.playBar(bar)
+
+
+Sequencer_sync() \
+    .withInstruments([LedLighter(), Printer()]) \
+    .playInLoop("X.xx.xx.X.x.x.x.")
